d8e3986dd49ecf7758e009771bd746229533829d
ducanh2710203 <duc2710203@gmail.com>
2023-04-03 22:23:20 +0700

    first commit

---
+++ lớp và đối tượng/recipe.js
@@ -0,0 +1,13 @@
+class Recipe {
+    title = "trứng rán"
+    servings = "3"
+    ingredients = ["6 quả trứng","10l dầu ăn","mắm tôm","chảo"]
+}
+let recipe = new Recipe()
+console.log("tên món ăn "+recipe.title)
+console.log("khẩu phần ăn "+recipe.servings+" người")
+console.log("thành phần ")
+for (let i = 0; i < recipe.ingredients.length; i++) {
+
+    console.log("-"+recipe.ingredients[i])
+}
