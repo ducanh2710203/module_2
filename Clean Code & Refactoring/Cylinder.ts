5d0c2dd85f13dd1ef09d47382330664b564b0d7f
ducanh2710203 <duc2710203@gmail.com>
2023-04-11 11:08:26 +0700

    ts

--- Clean Code & Refactoring/Cylinder.ts
+++ Clean Code & Refactoring/Cylinder.ts
@@ -1,7 +1,17 @@
+function getBaseArea(radius: number) {
+    let baseArea: number = Math.PI * radius * radius;
+    return baseArea;
+}
+
+function getPerimeter(radius: number) {
+    let perimeter: number = 2 * Math.PI * radius;
+    return perimeter;
+}
+
 export class Cylinder {
     public static getVolume(radius: number, height: number): number {
-        let baseArea: number = Math.PI * radius * radius;
-        let perimeter: number = 2 * Math.PI * radius;
+        let baseArea = getBaseArea(radius);
+        let perimeter = getPerimeter(radius);
         let volume: number = perimeter * height + 2 * baseArea;
         return volume;
     }
