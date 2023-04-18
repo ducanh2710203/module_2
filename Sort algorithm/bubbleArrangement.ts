724004c0bdd676926bda0803444a83349b73c17a
ducanh2710203 <duc2710203@gmail.com>
2023-04-18 09:50:50 +0700

    first commit

---
+++ Sort algorithm/bubbleArrangement.ts
@@ -0,0 +1,18 @@
+ let list: number[] = [2, 3, 2, 5, 6, 1, -2, 3, 14, 12];
+ let exam:boolean = true
+function bubbleSort(list: number[]) {
+    while (exam) {
+        exam = false
+        for (let i = 0; i < list.length; i++) {
+            if (list[i-1]>list[i]){
+                list.splice(i-1, 2, list[i], list[i-1]);
+                exam = true
+            }
+
+        }
+        }
+
+    return list
+}
+
+ console.log(bubbleSort(list))
